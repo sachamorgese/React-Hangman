68639ebb4f093144a9c2c515182792d4f880f38f
Sacha Morgese <sachamorgese@gmail.com>
2019-06-05 01:18:08 +0900

    First commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function (props) {
+  return (
+    <>
+      <span className="Home__text">Insert the word</span>
+      <input type="text" className="Home__input" />
+      <button type="button" className="Home__button">Done</button>
+    </>
+  )
+}
