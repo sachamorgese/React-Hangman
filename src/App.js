b23271709d21c91e0122216d7faf6628ee91a8c9
Sacha Morgese <sachamorgese@gmail.com>
2019-06-07 01:23:46 +0900

    The basic functionality of the app is there

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Home from './Home'
 
 function App() {
   const [newGame, setNewGame] = useState(true);
+  const [secretWord, setSecretWord] = useState('');
+  const [enteredLetters, setEnteredLetters] = useState({});
+  
+  useEffect(() => {
+    const handleKeyInput = (e) => {
+      console.log(enteredLetters)
+      setEnteredLetters(prevState => ({
+        ...prevState,
+        [e.key.toLowerCase()]: true
+        })
+      );
+    };
+    
+    if (!newGame) {
+      document.addEventListener('keypress', handleKeyInput)
+    } else {
+      document.removeEventListener('keypress', handleKeyInput)
+    }
+  }, [newGame]);
+  
+  const showLetters = secretWord => {
+    const { length } = secretWord;
+    return secretWord.split('').map((letter, i) => {
+      if (i === 0 || i === length - 1 || Boolean(enteredLetters[letter])) {
+        return <span key={i}>{`${letter}${i !== length - 1 ? ' ' : ''}`}</span>
+      } else {
+        return <span key={i}>_ </span>
+      }
+    });
+  };
   
   return (
     <div className="App">
       {
         newGame ?
-          <Home/> :
-          <div>Nohome</div>
+          <Home setNewGame={setNewGame} setSecretWord={setSecretWord} /> :
+          <div>{ showLetters(secretWord) }</div>
       }
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function (props) {
+function Home (props) {
+  const [ word, setWord ] = useState('');
+  const [ error, setError ] = useState(false);
+
+  const { setNewGame, setSecretWord } = props;
+  
+  const handleInput = (e) => {
+    const { value } = e.target;
+    if (value.length > 15) {
+      setError(true);
+      return;
+    }
+    setWord(value.toLowerCase());
+    setError(false)
+  };
+
+  const handleSubmit = (input) => {
+    const length = input.length;
+    
+    if(length >= 5 && length <= 15) {
+      setSecretWord(input);
+      setNewGame(false);
+    } else {
+      setError(true)
+    }
+  };
+  
   return (
     <>
-      <span className="Home__text">Insert the word</span>
-      <input type="text" className="Home__input" />
-      <button type="button" className="Home__button">Done</button>
+      <span className="Home__text">Insert the word</span>s
+      <input type="password" className="Home__input" onChange={handleInput} value={word} />
+      {error && <span>The word should be 5 and 15 characters long</span>}
+      <button type="button" className="Home__button" onClick={() => handleSubmit(word)}>Done</button>
     </>
   )
 }
+
+export default Home;
